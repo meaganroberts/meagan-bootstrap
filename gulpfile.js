2ccf46366c1982692233a264b21f5094802c98d0
Meagan Roberts <meagan.roberts@utexas.edu>
2018-10-30 16:36:22 -0500

    help my bootstrap is poop

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass        = require('gulp-sass');
 var gutil       = require('gulp-util');
 
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
         .on('error', gutil.log)
         .pipe(gulp.dest("docs/css"))
